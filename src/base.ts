34b77afca6416bbdb10aa94fdf9f7eb88797f5e1
Kyle Carbone <kcarbone@me.com>
2023-01-27 18:44:49 -0500

    first part of upgrade

--- src/base.ts
+++ src/base.ts
@@ -1,3 +1,4 @@
+import { Worker } from 'node:worker_threads';
 import { hrtime } from 'node:process';
 import gpio from 'array-gpio';
 import { getLogger } from 'bark-logger';
@@ -8,18 +9,33 @@ import { getLogger } from 'bark-logger';
 //   * Colors for output
 //   * Function param annotations
 //   * try 400kHz
+//   * Check for sudo when i2c is used
+//   * Add chunking (break with 18-byte array)
+//   * Refactor console.log squelch 
+//   * Mock device?
 
 class BaseDevice {
   private log = getLogger('base-device');
   private nsTime = BigInt(0);
+  private nsCount = BigInt(0);
+  private nsTotal = BigInt(0);
   protected i2c: any;
 
+  /**
+   * Sup?
+   * 
+   * @param i2cAddress - Address of the attached device
+   */
   constructor(i2cAddress?: number) {
     if (i2cAddress) {
       this.log.debug(`Connecting to i2c device at ${this.hex(i2cAddress)}...`);
       this.i2c = gpio.startI2C();
       this.i2c.selectSlave(i2cAddress);
-      this.i2c.setTransferSpeed(100000);
+
+      const tmp = console.log;
+      console.log = () => { };
+      this.i2c.setTransferSpeed(400000);
+      console.log = tmp;
     }
   }
 
@@ -29,24 +45,41 @@ class BaseDevice {
       .join(' ');
   }
 
+  protected timeParts(ticks: bigint) {
+    const k = BigInt(1000);
+    const units = ['ns', 'us', 'ms', 's'];
+    const parts = [];
+
+    for (let unit of units) {
+      if (ticks > 0) {
+        parts.unshift((ticks % k) + unit);
+        ticks = (ticks - (ticks % k)) / k;
+      }
+    }
+
+    return parts;
+  }
+
   protected startTimer() {
     this.nsTime = hrtime.bigint();
   }
 
   protected endTimer() {
     let ticks = hrtime.bigint() - this.nsTime;
-    const k = BigInt(1000);
-    const units = ['ns', 'us', 'ms', 's'];
-    const time = [];
+    this.nsCount++;
+    this.nsTotal += ticks;
+    const time = this.timeParts(ticks)[0];
+    const avg = this.timeParts(this.nsTotal / this.nsCount)[0];
 
-    for (let unit of units) {
-      if (ticks > 0) {
-        time.unshift((ticks % k) + unit);
-        ticks = (ticks - (ticks % k)) / k;
-      }
-    }
+    this.log.trace(''
+      + this.style('  Operation completed in ', 90)
+      + this.style(time, 1, 90)
+      + this.style(` (avg: ${avg})`, 90)
+    );
+  }
 
-    this.log.trace(`  Operation completed in ${time[0]}`);
+  protected style(str: string, ...codes: number[]) {
+    return `\x1b[${codes.join(';')}m${str}\x1b[0m`;
   }
 
   writeByte(register: number, byte: number) {
@@ -56,7 +89,7 @@ class BaseDevice {
   writeBlock(register: number, data: number[]) {
     this.startTimer();
     const buf = Buffer.from([register, ...data]);
-    this.log.trace(`WRITE (${this.hex(register)}): ${this.hex(data)}`);
+    this.log.trace(`${this.style('WRITE', 33)} (${this.hex(register)}): ${this.hex(data)}`);
     this.i2c.write(buf, buf.length);
     this.endTimer();
   }
--- tests/piglow.ts
+++ tests/piglow.ts
@@ -1,20 +1,29 @@
-import { BaseDevice } from "../src";
-import { Logger, Levels, config } from "bark-logger";
+import { env } from 'node:process';
+import { Logger, Levels, config } from 'bark-logger';
+import { BaseDevice } from '../src';
 
 const MODE_REGISTER = 0x00;
 const ENABLE_REGISTER = 0x13;
 const APPLY_REGISTER = 0x16;
+const RESET_REGISTER = 0x17;
 
 const log = new Logger('piglow-test');
-config.threshold = Levels.TRACE;
+config.threshold = env.LOGLEVEL ?? Levels.TRACE;
 
 log.info('Starting test');
 const piglow = new BaseDevice(0x54);
 
+piglow.writeByte(RESET_REGISTER, 1);
 piglow.writeByte(MODE_REGISTER, 1);
 piglow.writeBlock(ENABLE_REGISTER, [0b00111111, 0b00111111, 0b00111111]);
 
-piglow.writeByte(12, 0);
+piglow.writeBlock(1, [0, 0, 0, 0, 0, 0]);
+piglow.writeBlock(7, [0, 0, 0, 0, 0, 0]);
+piglow.writeBlock(13, [0, 0, 0, 0, 0, 0]);
+piglow.writeByte(12, 10);
+piglow.writeByte(4, 10);
+piglow.writeByte(9, 30);
+piglow.writeByte(1, 30);
 
 piglow.writeByte(APPLY_REGISTER, 0xFF);
 
